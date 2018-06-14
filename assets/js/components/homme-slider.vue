13c4082e4239882b335b36c5974967350cad2b2c
Baldrani <mael.mayon@free.fr>
2018-06-14 16:11:13 +0200

    Add Slider (without data from ddb)

---
+++ assets/js/components/homme-slider.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="http://www.placehold.it/700x300" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://www.placehold.it/700x300" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://www.placehold.it/700x300" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://www.placehold.it/700x300" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="http://www.placehold.it/700x300" alt="">
+            </div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+    </div>
+</template>
+
+<script>
+    import Swiper from 'swiper'
+    window.Swiper = Swiper
+
+    export default {
+        name: "homme-slider",
+        mounted: function () {
+            this.$nextTick(function () {
+                // Code that will run only after the
+                // entire view has been rendered
+                new Swiper('.swiper-container', {
+                    pagination: {
+                        el: '.swiper-pagination',
+                    },
+                });
+            })
+        }
+    }
+</script>
