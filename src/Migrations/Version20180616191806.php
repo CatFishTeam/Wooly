0a5fb12207e001e636868b01cf71c9b84786652a
guillaumepn <guillaumepn@free.fr>
2018-07-14 19:32:24 +0200

    add verification if user exists in levels

--- src/Migrations/Version20180616191806.php
+++ src/Migrations/Version20180616191806.php
@@ -16,9 +16,8 @@ final class Version20180616191806 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE fos_user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', UNIQUE INDEX UNIQ_957A647992FC23A8 (username_canonical), UNIQUE INDEX UNIQ_957A6479A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_957A6479C05FB297 (confirmation_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE level (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, data JSON NOT NULL, best JSON DEFAULT NULL, thumbnail VARCHAR(255) NOT NULL, status INT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, played INT NOT NULL, finished INT DEFAULT NULL, INDEX IDX_9AEACC13A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE level (id INT AUTO_INCREMENT NOT NULL, user_id INT NULL, data JSON NOT NULL, best JSON DEFAULT NULL, thumbnail VARCHAR(255) NOT NULL, status INT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, played INT NOT NULL, finished INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `character` (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, level INT NOT NULL, INDEX IDX_937AB0349D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE level ADD CONSTRAINT FK_9AEACC13A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)');
         $this->addSql('ALTER TABLE `character` ADD CONSTRAINT FK_937AB0349D86650F FOREIGN KEY (user_id_id) REFERENCES fos_user (id)');
     }
 
@@ -27,7 +26,6 @@ final class Version20180616191806 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE level DROP FOREIGN KEY FK_9AEACC13A76ED395');
         $this->addSql('ALTER TABLE `character` DROP FOREIGN KEY FK_937AB0349D86650F');
         $this->addSql('DROP TABLE fos_user');
         $this->addSql('DROP TABLE level');
